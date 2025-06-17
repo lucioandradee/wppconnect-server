5f5169d6cec43797b41c08c145c5d2d1cb5e4883
Lúcio Aguiar Andrade <lucioa.andrade@gmail.com>
2025-06-17 11:51:22 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { ServerOptions } from './types/ServerOptions';
 
 export default {
-  secretKey: 'THISISMYSECURETOKEN',
+  secretKey: 'UjadnUoqe#PlQ25/3o0',
   host: 'http://localhost',
   port: '21465',
   deviceName: 'WppConnect',
